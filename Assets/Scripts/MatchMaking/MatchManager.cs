9c68355d94ba9c13023a128b8a49e22e0fd48119
yujinshin <raki2001@o365.yonsei.ac.kr>
2021-01-07 21:11:41 +0900

    Notify player exit on disable

--- Assets/Scripts/MatchMaking/MatchManager.cs
+++ Assets/Scripts/MatchMaking/MatchManager.cs
@@ -1,22 +1,41 @@
 ﻿using BattleCampusMatchServer.Models;
 using BattleCampusMatchServer.Models.DTOs;
 using System.Collections;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Assets.Scripts.MatchMaking
 {
     public class MatchManager : MonoBehaviour
     {
+        public static MatchManager Instance;
+
+        public MatchDTO Match { get; private set; }
         public IpPortInfo IpPortInfo { get; private set; } = new IpPortInfo();
 
         void Start()
         {
             DontDestroyOnLoad(this);
+
+            Instance = this;
         }
 
         public void ConfigureMatchInfo(MatchDTO match)
         {
+            Match = match;
             IpPortInfo = match.IpPortInfo;
+        }
+
+        //This means the player has exited game or that the game is ended.
+        //This is not synced as it's not a NetworkBehavior
+        private async void OnDisable()
+        {
+            await NotifyPlayerExitAsync();
+        }
+
+        public async Task NotifyPlayerExitAsync()
+        {
+            await MatchServer.Instance.NotifyPlayerExitAsync(IpPortInfo.IpAddress, Match.MatchID, UserManager.Instacne.User);
         }
     }
 }
