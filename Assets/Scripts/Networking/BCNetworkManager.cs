5a52c2c8e9d25e18a86142e3514ddc1acc5f0f1d
yujinshin <raki2001@o365.yonsei.ac.kr>
2021-01-07 21:13:10 +0900

    Small resturcturing and now gets its real internal ip

--- Assets/Scripts/Networking/BCNetworkManager.cs
+++ Assets/Scripts/Networking/BCNetworkManager.cs
@@ -4,6 +4,8 @@ using Mirror;
 using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using UnityEngine;
 
 namespace Assets.Scripts.Networking
@@ -14,6 +16,12 @@ namespace Assets.Scripts.Networking
         {
             base.OnStartServer();
 
+            networkAddress = Dns.GetHostEntry(Dns.GetHostName())
+                .AddressList.First((x) =>
+                {
+                    return x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork;
+                }).ToString();
+
             //TOOD : Send server open api call to the match server
             var serverInfo = new IpPortInfo
             {
@@ -21,25 +29,34 @@ namespace Assets.Scripts.Networking
                 DesktopPort = 7777,
                 WebsocketPort = 7778,
             };
-            
+
+            Debug.Log("IP : " + networkAddress);
+
             var name = $"Server:{networkAddress}";
 
-            MatchServer.Instance.RegisterServerAsync(name, serverInfo).ContinueWith (t =>
-            {
-                if (t.Result == false)
-                {
+            MatchServer.Instance.RegisterServerAsync(name, serverInfo).ContinueWith(t =>
+           {
+               if (t.Result == false)
+               {
                     //TODO : Shutdown server instance and report it.
                     //Shutdown();
                 }
-            });
+           });
+        }
+
+        public override void OnClientDisconnect(NetworkConnection conn)
+        {
+            base.OnClientDisconnect(conn);
+
+            Debug.LogError("Disconnect");
         }
 
         public override async void OnStopServer()
         {
-            base.OnStopServer();
-
             //TODO : Make graceful stop.
             await MatchServer.Instance.UnRegisterServerAsync(networkAddress);
+
+            base.OnStopServer();
         }
     }
 }
