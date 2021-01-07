5cc8dc2bd5cea739ff059b0b90f9e74af8fc6ea1
yujinshin <raki2001@o365.yonsei.ac.kr>
2021-01-07 21:08:03 +0900

    Users are added

---
+++ Assets/Scripts/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleCampusMatchServer.Models
+{
+    public class User
+    {
+        public Guid ID { get; set; }
+        public string StudentID { get; set; }
+        public string Name { get; set; }
+    }
+}
