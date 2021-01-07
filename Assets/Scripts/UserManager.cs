0f89023bfd610d5c1439efddff6b72a39a2e737a
yujinshin <raki2001@o365.yonsei.ac.kr>
2021-01-07 21:09:19 +0900

    Added UserManager that keep track of who this user is

---
+++ Assets/Scripts/UserManager.cs
@@ -0,0 +1,19 @@
+﻿using BattleCampusMatchServer.Models;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class UserManager : MonoBehaviour
+    {
+        public static UserManager Instacne;
+        public User User { get; private set; } = new GuestUser();
+
+        private void Start()
+        {
+            Instacne = this;
+
+            DontDestroyOnLoad(this);
+        }
+    }
+}
