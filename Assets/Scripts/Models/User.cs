e84faf3d4dbf13d48b8591d4e3867d6876c7220d
yujinshin <raki2001@o365.yonsei.ac.kr>
2021-01-11 17:47:53 +0900

    User field update and login screen on working

--- Assets/Scripts/Models/User.cs
+++ Assets/Scripts/Models/User.cs
@@ -4,11 +4,16 @@ using System.Text;
 
 namespace BattleCampusMatchServer.Models
 {
+    [Serializable]
     public class User
     {
         public Guid ID { get; set; }
         public string StudentID { get; set; }
         public string Name { get; set; }
+        public string Token { get; set; }
+        //Joining match ID
+        public string MatchID { get; set; }
         public bool IsHost { get; set; } = false;
+        public int ConnectionID { get; set; } = -1;
     }
 }
